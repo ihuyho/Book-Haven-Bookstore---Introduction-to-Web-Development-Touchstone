3e51a9e50807c9e8a92add27f2afca1ac40014fc
Duc Huy Ho <hoduchuy.n@gmail.com>
2024-11-16 12:05:23 -0500

    Finish drink order program

--- Introduction to Java/Practice/DrinkOrder.java
+++ Introduction to Java/Practice/DrinkOrder.java
@@ -40,9 +40,67 @@ public class DrinkOrder {
         }
         else if (choice == 2) {
             drinkDetails = "Coffee";
+            System.out.println("Regular or Decaf?");
+            System.out.println("1. Regular\n2. Decaf");
+            int coffeeChoice = input.nextInt();
+
+            if (coffeeChoice == 1) {
+                drinkDetails = drinkDetails + ", Regular";
+            }  
+            else if (coffeeChoice == 2) {
+                drinkDetails = drinkDetails + ". Decaf";
+            }
+            else {
+                System.out.println("Sorry, not a valid option.");
+            }
+            
+            System.out.println("Do you want milk, cream, or none?");
+            System.out.println("1. Milk\n2. Cream\n3. None");
+            int milkChoice = input.nextInt();
+
+            if (milkChoice == 1) {
+                drinkDetails = drinkDetails + ", Milk";
+            }
+            else if (milkChoice == 2) {
+                drinkDetails = drinkDetails + ", Cream";
+            }
+            else if (milkChoice == 3) {
+                drinkDetails = drinkDetails + ", No Milk or Cream";
+            }
+            else {
+                System.out.println("Sorry, not a valid option.");
+            }
+
+            System.out.println("Do you want sugar or no sugar?");
+            System.out.println("1. Sugar\n2. No Sugar");
+            int sugarChoice = input.nextInt();
+
+            if (sugarChoice == 1) {
+                drinkDetails = drinkDetails + ", Sugar";
+            }
+            else if (sugarChoice == 2) {
+                drinkDetails = drinkDetails + ", No Sugar";
+            }
+            else {
+                System.out.println("Sorry, not a valid option.");
+            }
+
         }
         else if (choice == 3) {
             drinkDetails = "Tea";
+            System.out.println("Do you want green tea or black tea?");
+            System.out.println("1. Green\n2. Black");
+            int teaChoice = input.nextInt();
+
+            if (teaChoice == 1) {
+                drinkDetails = drinkDetails + "Green";
+            }
+            else if (teaChoice == 2) {
+                drinkDetails = drinkDetails + "Black";
+            }
+            else {
+                System.out.println("Sorry, not a valid option.");
+            }
         }
         else {
             System.out.println("Sorry, not a valid drink selection.");
